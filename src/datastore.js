8a6a9c7f01f7fb4eba69730d56d18bad344d5ad0
Fernando Rodriguez Sela <frsela@tid.es>
2012-06-13 12:33:14 +0200

    Added MongoDB support as storage engine

--- src/datastore.js
+++ src/datastore.js
@@ -6,13 +6,38 @@
  */
 
 var redis = require("redis");
+var mongodb = require("mongodb");
 var server_info = require("./config.js").server_info;
 
 function datastore() {
-  console.log("REDIS based data store loaded.");
+  console.log("MONGO/REDIS based data store loaded.");
 
-  this.redis = redis.createClient();
+  // In-Memory storage
   this.nodesTable = {};
+
+  // Connection to REDIS
+  this.redis = redis.createClient();
+
+  // Connection to MongoDB
+  this.db = new mongodb.Db(
+    "push_notification_server",
+    new mongodb.Server(
+      "127.0.0.1",
+      27017,
+      {auto_reconnect: true, poolSize: 4}
+    ),
+    {native_parser: false}
+  );
+
+  // Establish connection to db
+  this.db.open(function(err, db) {
+    if(err == null) {
+      console.log("Connected to MongoDB !");
+    } else {
+      console.log("Error connecting to MongoDB !");
+      // TODO: Cierre del servidor? Modo alternativo?
+    }
+  });
 }
 
 datastore.prototype = {
@@ -29,14 +54,23 @@ datastore.prototype = {
     this.nodesTable[token] = connector;
 
     // Register in REDIS that this server manages this node
-    this.redis.set("node_" + token, server_info.key);
+    this.redis.set("node_" + token, server_info.id);
+
+    // Register in MONGO that this server manages this node
+    this.db.collection("nodes", function(err, collection) {
+      collection.insert( { 'token': token, 'serverId': server_info.id }, function(err,d) {
+        if(err != null)
+          console.log("Node inserted into MongoDB");
+        else
+          console.log("Error inserting node into MongoDB");
+      });
+    });
   },
 
   /**
    * Gets a node connector
    */
   getNode: function (token) {
-    console.log("Server: " + this.redis.get("node_" + token));
     if(this.nodesTable[token]) {
       return this.nodesTable[token];
     }
@@ -48,27 +82,30 @@ datastore.prototype = {
    * Register a new application
    */
   registerApplication: function (appToken, nodeToken) {
+    // Store in REDIS
     this.redis.sadd("app_" + appToken, nodeToken);
+
+    // Store in MongoDB
+    this.db.collection("apps", function(err, collection) {
+      collection.insert( { 'token': appToken, 'node': nodeToken }, function(err,d) {
+        if(err != null)
+          console.log("Application inserted into MongoDB");
+        else
+          console.log("Error inserting application into MongoDB");
+      });
+    });
+
   },
 
   /**
    * Gets an application node list
    */
-  getApplication: function (token) {
-    this.redis.smembers("app_" + token, function(err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-    });
-/*
-    if(this.appsTable[token]) {
-      return this.appsTable[token];
-    }
-    return false;    
-  }
-*/
-    return false;
+  getApplication: function (token, cbfunc) {
+    // Get from REDIS
+    this.redis.smembers("app_" + token, cbfunc);
+
+    // Get from MongoDB
+    // TODO
   }
 }
 
